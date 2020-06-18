d500b14b2858cadedd915377507590a65ef3a0ba
Michael Clark <clark.michael.c@gmail.com>
2020-06-17 20:54:23 -0600

    Adding BIP-39 decoding as well

--- bip39.php
+++ bip39.php
@@ -1,42 +1,24 @@
 <?php
 
 // works for all values 128-256
-function addChecksum($bytes) {
-	return $bytes . hex2bin(substr(hash('sha256', $bytes), 0, 2));
+function addChecksum($data) {
+	$bits = strlen($data) / 4;
+	$bytes = (int) ceil($bits / 8);
+	$checksum = hex2bin(substr(hash('sha256', $data), 0, 2*$bytes));
+
+	$hole = $bytes * 8 - $bits;
+	$mask = (pow(2, $bytes * 8) - 1) & ~(pow(2, $hole) - 1);
+	$checksum = ord($checksum) & $mask;
+
+	return $data . chr($checksum);
 }
 
-// works for 128-256 bits of entropy; 12 - 24 words
-// https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
-function bip39($dict, $len, $rand = null) {
-	$index_bits = (int) floor(log(count($dict), 2));
-
-	$generated = false;
-	if (!$rand) {
-		$bytes = ceil(($len * $index_bits) / 8);
-		$rand = addChecksum(random_bytes($bytes)); // 128 bits, +4 from checksum
-		$generated = true;
-	} else {
-		// A bit stricter than the standard - always requires a full 2 bytes, works from 128-256 bits
-		if ($rand !== addChecksum(substr($rand, 0, -1)))
-			throw new Exception('Checksum doesn\'t match');
-		$bytes = $len;
-		$len = floor(($bytes * 8) / $index_bits);
-	}
-
-	if ($bytes < 16 || $bytes > 32)
-		throw new Exception("We can only handle 128-256 bits of entropy for BIP-39 right now");
-
-
-	if ($generated)
-		return [run($dict, $bytes, $rand), $rand];
-	else
-		return run($dict, $bytes, $rand);
-}
-
-function run($dict, $len, $rand = null) {
+function encode($dict, $len, $rand = null) {
 	$index_bits = (int) floor(log(count($dict), 2));
 	if ($index_bits < 8)
 		throw new Exception("If you need less than 255 dictionary entries, rewrite the function");
+	if (count($dict) !== pow(2, $index_bits))
+		throw new Exception("Power of 2 dictionaries, or rewrite the function please");
 	$mask = pow(2, $index_bits) - 1;
 
 	$generated = false;
@@ -80,10 +62,123 @@ function run($dict, $len, $rand = null) {
 		return implode(' ', $words);
 }
 
+// works for 128-256 bits of entropy; 12 - 24 words
+// https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
+function bip39encode($dict, $len, $rand = null) {
+	$index_bits = (int) floor(log(count($dict), 2));
+
+	$generated = false;
+	if (!$rand) {
+		$bytes = ceil(($len * $index_bits) / 8);
+		$rand = addChecksum(random_bytes($bytes)); // 128 bits, +4 from checksum
+		$generated = true;
+	} else {
+		$checksummed = addChecksum(substr($rand, 0, -1));
+		if ($rand !== $checksummed) {
+			throw new Exception('Checksum doesn\'t match, should end in ' . bin2hex(substr($checksummed, -1, 1)));
+		}
+		$bytes = $len;
+		$len = floor(($bytes * 8) / $index_bits);
+	}
+
+	if ($bytes < 16 || $bytes > 32)
+		throw new Exception("We can only handle 128-256 bits of entropy for BIP-39 right now");
+
+
+	if ($generated)
+		return [encode($dict, $bytes, $rand), $rand];
+	else
+		return encode($dict, $bytes, $rand);
+}
+
+function decode($str, $dict) {
+	$index_bits = (int) floor(log(count($dict), 2));
+	if ($index_bits < 8)
+		throw new Exception("If you need less than 255 dictionary entries, rewrite the function");
+	if (count($dict) !== pow(2, $index_bits))
+		throw new Exception("Power of 2 dictionaries, or rewrite the function please");
+
+	$bytes = (int) floor($index_bits / 8);
+	$bits = $index_bits % 8;
+	//var_dump(compact('bytes', 'bits'));
+
+	$words = explode(' ', $str);
+	$out = '';
+
+	$pos = 0;
+	$rest = 0;
+	for ($i = 0; $i < count($words); $i++) {
+		$found = array_search($words[$i], $dict);
+		if ($found === false)
+			return false;
+
+		//if ($index_bits == 11) var_dump($words[$i], $found);
+
+		$j = $index_bits;
+		while ($j > 0) {
+			// how much do I have left? How much to fill in prior byte?
+
+			// mask off already used top bits
+			$from = $found & (pow(2, $j) - 1);
+
+			if ($pos + $j >= 8) {
+				// We'll finish a byte
+
+				$count = 8 - $pos; // how many to grab
+
+				// grab $count bits starting from $j
+				$val = $from >> ($j - $count);
+
+				$rest += $val;
+
+				$out .= chr($rest);
+				$rest = 0;
+
+				$pos = 0;
+				$j -= $count;
+			} else {
+				// We won't finish a byte; less than 8 bits remain
+				$rest += $from << ((8 - $pos) - $j);
+				$pos += $j;
+				$j = 0;
+			}
+		}
+		/*
+		for ($j = $bytes - 1; $j >= 0; $j--) {
+			$out .= chr($found >> ((8 * $j) + $bits));
+		}
+		if ($bits) {
+			if ($rest === null)
+				$rest = 0;
+			if ($pos + $bits < 8) {
+				$rest += ($found & (255 >> (8 - $bits))) << ((8 - $pos) - $bits);
+				$pos += $bits;
+			} else {
+			}
+		}
+		 */
+	}
+	if ($pos)
+		$out .= chr($rest);
+
+	return $out;
+}
+
+function bip39decode($str, $dict) {
+	$binary = decode($str, $dict);
+	if ($binary === false)
+		return false;
+	$checksummed = addChecksum(substr($binary, 0, -1));
+	if ($binary !== $checksummed) {
+		throw new Exception('Checksum doesn\'t match, should end in ' . bin2hex(substr($checksummed, -1, 1)));
+	}
+	return $binary;
+}
+
 if (get_included_files()[0] == __FILE__) {
 	$dict = file(__DIR__ . '/english.txt', FILE_IGNORE_NEW_LINES);
 
-	list($words, $rand) = bip39($dict, 12);
+	list($words, $rand) = bip39encode($dict, 12);
 	echo "words: " . $words . "\n";
 	echo "hex: " . bin2hex($rand) . "\n";
 }
--- test.php
+++ test.php
@@ -6,7 +6,7 @@ require('bip39.php');
 
 function mustEqual($a, $b, $exp = '') {
 	if ($a !== $b) {
-		echo ($exp ? $exp . ': ' : '') . var_export($a, true) . ' !== ' . var_export($b, true);
+		echo ($exp ? $exp . ': ' : '') . bin2hex($a) . ' !== ' . bin2hex($b) . "\n";
 	}
 }
 
@@ -16,39 +16,103 @@ function mustThrow($a, $exp = '') {
 	} catch (Exception $e) {
 		return;
 	}
-	echo ($exp ? $exp . ': ' : '') . var_export($a, true) . ' did not throw an exception';
+	echo ($exp ? $exp . ': ' : '') . var_export($a, true) . " did not throw an exception\n";
 }
 
-$base8 = range(0, 255);
-mustEqual(run($base8, 2, "\x00\x00"), implode(' ', [$base8[0], $base8[0]]), 'base 8');
-mustEqual(run($base8, 2, "\x00\xFF"), implode(' ', [$base8[0], $base8[255]]), 'base 8');
-mustEqual(run($base8, 2, "\xFF\xFF"), implode(' ', [$base8[255], $base8[255]]), 'base 8');
-mustEqual(run($base8, 4, "\x01\x02\x03\x04"), implode(' ', [$base8[1], $base8[2], $base8[3], $base8[4]]), 'base 8');
+$base8 = array_map(fn($x) => strval($x), range(0, 255));
+$base11 = array_map(fn($x) => strval($x), range(0, 2047));
+$base20 = array_map(fn($x) => strval($x), range(0, pow(2, 20) - 1));
 
-$base11 = range(0, 2047);
-mustEqual(run($base11, 2, "\x00\x00"), implode(' ', [$base11[0]]), 'base 11');
-mustEqual(run($base11, 2, "\x00\x1F"), implode(' ', [$base11[0]]), 'base 11');
-mustEqual(run($base11, 2, "\xFF\xFF"), implode(' ', [$base11[2047]]), 'base 11');
-mustEqual(run($base11, 3, "\x00\x00\x00"), implode(' ', [$base11[0], $base11[0]]), 'base 11');
-mustEqual(run($base11, 3, "\x00\x00\x03"), implode(' ', [$base11[0], $base11[0]]), 'base 11');
-mustEqual(run($base11, 3, "\xFF\xFF\xFF"), implode(' ', [$base11[2047], $base11[2047]]), 'base 11');
-
-mustEqual(run($base11, 5, "\x00\x20\x08\x01\x80"), implode(' ', [$base11[1], $base11[2], $base11[3]]), 'base 11');
-mustEqual(count(explode(' ', run($base11, 12)[0])), 12);
+// encode
 
 // Limitations
-mustThrow(fn() => run(range(0, 1), 1));
-mustEqual(count(explode(' ', run(range(0, pow(2, 20)-1), 3)[0])), 3);
+mustThrow(fn() => encode(range(0, 1), 1));
+mustThrow(fn() => encode(range(0, 256), 1));
+
+mustEqual(encode($base8, 2, "\x00\x00"), implode(' ', [$base8[0], $base8[0]]), 'base 8');
+mustEqual(encode($base8, 2, "\x00\xFF"), implode(' ', [$base8[0], $base8[255]]), 'base 8');
+mustEqual(encode($base8, 2, "\xFF\xFF"), implode(' ', [$base8[255], $base8[255]]), 'base 8');
+mustEqual(encode($base8, 4, "\x01\x02\x03\x04"), implode(' ', [$base8[1], $base8[2], $base8[3], $base8[4]]), 'base 8');
+
+mustEqual(encode($base11, 2, "\x00\x00"), implode(' ', [$base11[0]]), 'base 11');
+mustEqual(encode($base11, 2, "\x00\x1F"), implode(' ', [$base11[0]]), 'base 11');
+mustEqual(encode($base11, 2, "\xFF\xFF"), implode(' ', [$base11[2047]]), 'base 11');
+mustEqual(encode($base11, 3, "\x00\x00\x00"), implode(' ', [$base11[0], $base11[0]]), 'base 11');
+mustEqual(encode($base11, 3, "\x00\x00\x03"), implode(' ', [$base11[0], $base11[0]]), 'base 11');
+mustEqual(encode($base11, 3, "\xFF\xFF\xFF"), implode(' ', [$base11[2047], $base11[2047]]), 'base 11');
+
+mustEqual(encode($base11, 5, "\x00\x20\x08\x01\x80"), implode(' ', [$base11[1], $base11[2], $base11[3]]), 'base 11');
+mustEqual(count(explode(' ', encode($base11, 12)[0])), 12);
+
+// very large dictionary
+mustEqual(count(explode(' ', encode($base20, 3)[0])), 3);
 
 // BIP 39 - uses a checksum, otherwise basically the same
 $bipdict = file(__DIR__ . '/english.txt', FILE_IGNORE_NEW_LINES);
 mustEqual(
-	bip39($bipdict, 17, hex2bin("400131ddbc4019ea172ee84649bbdd7f66")),
+	bip39encode($bipdict, 17, hex2bin("0000000000000000000000000000000030")),
+	"abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+);
+mustEqual(
+	bip39encode($bipdict, 17, hex2bin("ffffffffffffffffffffffffffffffff50")),
+	"zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong"
+);
+mustEqual(
+	bip39encode($bipdict, 25, hex2bin("ffffffffffffffffffffffffffffffffffffffffffffffff44")),
+	"zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo when"
+);
+mustEqual(
+	bip39encode($bipdict, 17, hex2bin("400131ddbc4019ea172ee84649bbdd7f60")),
 	"divorce another jazz joy account vital frequent tackle edge evidence warrior yard"
 );
 mustEqual(
-	bip39($bipdict, 17, addChecksum(hex2bin("400131ddbc4019ea172ee84649bbdd7f"))),
+	bip39encode($bipdict, 17, addChecksum(hex2bin("400131ddbc4019ea172ee84649bbdd7f"))),
 	"divorce another jazz joy account vital frequent tackle edge evidence warrior yard"
 );
-mustThrow(fn() => bip39($bipdict, 17, hex2bin("400131ddbc4019ea172ee84649bbdd7f00")), 'bad checksum');
-mustEqual(count(explode(' ', bip39($bipdict, 12)[0])), 12);
+mustThrow(fn() => bip39encode($bipdict, 17, hex2bin("400131ddbc4019ea172ee84649bbdd7f00")), 'bad checksum');
+mustEqual(count(explode(' ', bip39encode($bipdict, 12)[0])), 12);
+
+
+// decode
+
+// Limitations
+mustThrow(fn() => decode('0', range(0, 1)));
+mustThrow(fn() => decode('0', range(0, 256)));
+mustEqual(decode('lolnope', $base8), false);
+
+mustEqual(decode(implode(' ', [$base8[0]]), $base8), "\x00");
+mustEqual(decode(implode(' ', [$base8[0], $base8[0]]), $base8), "\x00\x00");
+mustEqual(decode(implode(' ', [$base8[0], $base8[1]]), $base8), "\x00\x01");
+mustEqual(decode(implode(' ', [$base8[255], $base8[255]]), $base8), "\xFF\xFF");
+mustEqual(decode(implode(' ', [$base8[1], $base8[2], $base8[3], $base8[4]]), $base8), "\x01\x02\x03\x04");
+
+mustEqual(decode(implode(' ', [$base11[0]]), $base11), "\x00\x00");
+mustEqual(decode(implode(' ', [$base11[0], $base11[0]]), $base11), "\x00\x00\x00");
+mustEqual(decode(implode(' ', [$base11[1]]), $base11), "\x00\x20");
+mustEqual(decode(implode(' ', [$base11[2047]]), $base11), "\xFF\xE0");
+mustEqual(decode(implode(' ', [$base11[2047], $base11[2047]]), $base11), "\xFF\xFF\xFC");
+mustEqual(decode(implode(' ', [$base11[1], $base11[2], $base11[3]]), $base11), "\x00\x20\x08\x01\x80");
+
+mustEqual(decode(implode(' ', [$base20[1], $base20[pow(2, 20) - 1]]), $base20), "\x00\x00\x1f\xff\xff");
+
+// BIP 39
+mustEqual(
+	bip39decode("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about", $bipdict),
+	hex2bin("0000000000000000000000000000000030")
+);
+mustEqual(
+	bip39decode("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong", $bipdict),
+	hex2bin("ffffffffffffffffffffffffffffffff50")
+);
+mustEqual(
+	bip39decode("zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo when", $bipdict),
+	hex2bin("ffffffffffffffffffffffffffffffffffffffffffffffff44")
+);
+mustEqual(
+	bip39decode("divorce another jazz joy account vital frequent tackle edge evidence warrior yard", $bipdict),
+	hex2bin("400131ddbc4019ea172ee84649bbdd7f60"),
+);
+
+mustEqual(decode(encode($base8, 2, "\x00\x00"), $base8), "\x00\x00");
+mustEqual(decode(encode($base8, 2, "\xff\xff"), $base8), "\xff\xff");
+mustEqual(decode(encode($base11, 5, "\x00\x20\x08\x01\x80"), $base11), "\x00\x20\x08\x01\x80");
